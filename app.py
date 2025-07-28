354d647c017edb7adfaedf98efc936f869708035
Tyler Bosford <tylerbosford@icloud.com>
2025-07-28 11:30:30 +0000

    #1. Minor change to index and addition of db_test

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import psycopg2
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World from Tyler Bosford in 3308'
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://basketball_names_user:coWuQ7WjVRtH4r9uhIEdsC0Rdac7hDTo@dpg-d23lqpvdiees739os0og-a/basketball_names")
+    
